ae120b0fe3b5e9fbce971a3a4856fbef13623ddc
knowhatelse <kenan.kajtazovic.01@gmail.com>
2024-01-10 22:57:49 +0100

    Create the web page

--- mood-melody.client/src/App.js
+++ mood-melody.client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import FileUpload from './components/FileUpload/FileUpload';
+import Footer from './components/Footer/Footer';
+import SongsList from './components/SongsList/SongsList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-content-holder'>
+      <Navbar></Navbar>
+      <FileUpload></FileUpload>
+      {/*<SongsList></SongsList> */}
+      <Footer></Footer>
     </div>
   );
 }
